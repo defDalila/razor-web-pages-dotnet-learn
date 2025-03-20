c5c90f6043acfb673a9d8010e5d669d6d501ff68
defdalila <defdalila@outlook.com>
2025-03-20 13:46:56 -0300

    feat(site): update branding and add new page for books list

---
+++ Site/Pages/Livros.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Site.Pages
+{
+    public class LivrosModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
