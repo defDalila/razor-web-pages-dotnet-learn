fad30f80b0f4acf1e8a022e0b49ae51ad1c51657
defdalila <defdalila@outlook.com>
2025-03-20 09:45:51 -0300

    feat(migrations): add Livro entity and update gitignore

---
+++ Site/Migrations/20250320123802_CriarConntext.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Site.Migrations
+{
+    /// <inheritdoc />
+    public partial class CriarConntext : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Livros",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Titulo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Autor = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Genero = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Editora = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    TotalPaginas = table.Column<int>(type: "int", nullable: false),
+                    DataPublicacao = table.Column<DateOnly>(type: "date", nullable: false),
+                    Avaliacao = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Livros", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Livros");
+        }
+    }
+}
--- Site/Models/Livro.cs
+++ Site/Models/Livro.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,10 +9,11 @@ namespace Site.Models;
 public class Livro
 {
     public int Id { get; set; }
-    public string Titulo { get; set; }
-    public string Autor { get; set; }
-    public string Editora { get; set; }
-    public int Ano { get; set; }
-    public double Rating { get; set; }
-    public long TotalAvaliacoes { get; set; }
+    public required string Titulo { get; set; }    
+    public string? Autor { get; set; }
+    public string? Genero { get; set; }
+    public string? Editora { get; set; }
+    public int TotalPaginas { get; set; }
+    public DateOnly DataPublicacao { get; set; }
+    public double Avaliacao { get; set; }
 }
